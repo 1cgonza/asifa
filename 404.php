848231d24ba0c05e5d73778064df4ff16e6410d1
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-12-02 12:17:35 -0500

    move main tags to header and footer templates

--- 404.php
+++ 404.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-<main id="main" class="site-main" role="main">
   <article class="error-404">
     <header class="article-header">
       <h1>404</h1>
@@ -9,5 +8,4 @@
       <?php get_search_form(); ?>
     </section>
   </article>
-</main>
 <?php get_footer(); ?>
