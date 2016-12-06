d6ad15998fdec163fdfdab6e925127b900c27a52
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-12-05 20:52:11 -0500

    init JS, add Isotope and imagesloaded libs

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,4 +1,27 @@
+import Isotope from 'isotope-layout';
+import imagesLoaded from 'imagesloaded';
+
 (function() {
   'use strict';
-  
+
+  var galleries = document.querySelectorAll('.asifa-gallery');
+
+  galleries.forEach(function(gallery) {
+    imagesLoaded(gallery, function() {
+      var preloader = gallery.querySelector('.preloader');
+      var container = gallery.querySelector('.gallery-wrapper');
+
+      var isotope = new Isotope(container, {
+        itemSelector: '.gallery-item',
+        percentPosition: true,
+        masonry: {
+          columnWidth: '.gallery-item'
+        }
+      });
+
+      preloader.classList.add('hidden');
+      container.classList.remove('hidden');
+
+    });
+  });
 })();
