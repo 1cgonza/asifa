234457b2c752d49638738619a12011e4ebdb6e2a
Juan Camilo Gonzalez <info@juancgonzalez.com>
2017-01-12 22:23:00 -0500

    add header image

--- layouts/content-page.php
+++ layouts/content-page.php
@@ -1,13 +1,13 @@
 <article <?php post_class(); ?>>
-  <header class="article-header">
-    <h1><?php the_title(); ?></h1>
+  <header class="article-content-wrapper page-header bg-highlight-1">
+    <?php the_title('<h1 class="page-title ' . asifa_wrapper_class() . '">', '</h1>'); ?>
+    <?php
+    if ( has_post_thumbnail() ) : ?>
+      <div class="page-header-img" style="background-image:url(<?php the_post_thumbnail_url(); ?>)"></div>
+    <?php endif ?>
   </header>
 
-  <section class="article-section article-content">
+  <section class="article-section article-content <?php echo asifa_wrapper_class(); ?>">
     <?php the_content(); ?>
   </section>
-
-  <footer class="article-footer">
-    <p class="tags"><?php the_tags('', ', ', ''); ?></p>
-  </footer>
 </article>
